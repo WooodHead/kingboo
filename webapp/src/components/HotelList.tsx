6c2dfbe95894ce9f587f13ddc2ea62a2510c2858
piotrwestphal <piotr.westphal@gmail.com>
2020-07-10 23:21:46 +0200

    create webapp first scratch

---
+++ webapp/src/components/HotelList.tsx
@@ -0,0 +1,7 @@
+import React, {Suspense} from 'react';
+
+export default function HotelList() {
+  return (
+    <div>HotelList</div>
+  )
+}
---
+++ webapp/src/components/Test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Test.scss';
+
+export default function Test() {
+  return (
+    <div className="Test">
+      <p>Test</p>
+    </div>
+  );
+}
